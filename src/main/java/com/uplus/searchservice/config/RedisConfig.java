6f1e4c6342c3f0f79a9c0768c660bb93fed7ef26
avocado51 <ujmikzz7@hanmail.net>
2022-07-23 16:34:22 +0900

    Add redis cache config

--- src/main/java/com/uplus/searchservice/config/RedisConfig.java
+++ src/main/java/com/uplus/searchservice/config/RedisConfig.java
@@ -1,13 +1,20 @@
 package com.uplus.searchservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 ////////////////////////////////////
 // Create Date: 2022.07.22        //
@@ -15,6 +22,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 ///////////////////////////////////
 @Configuration
 @EnableRedisRepositories
+@EnableCaching
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -35,5 +43,21 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager() {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer())) // key는 byte[] 를 string으로 변환해서 사용
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer())) // value는 byte[] 를 json으로 변환해서 사용
+                .entryTtl(Duration.ofHours(24L)); // 캐시 ttl을 one-day로 지정
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(configuration)
+                .build();
     }
 }
